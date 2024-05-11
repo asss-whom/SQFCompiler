c2833999c43ef04585fb1a30a4305646b2a57dea
asss-whom <3059749821@qq.com>
2024-05-11 23:09:09 +0800

    Update utils.py
    Many update(e.g. Add support to dict).

--- utils.py
+++ utils.py
@@ -73,53 +73,269 @@ def translate(node: ast.AST) -> str:
     return ""
 
 
-@translate.register(ast.Module)
-@translate.register(ast.Interactive)
-@translate.register(ast.Expression)
+@translate.register
 def _(node: ast.Module | ast.Interactive | ast.Expression) -> str:
     return "".join(translate(child) for child in ast.iter_child_nodes(node))
 
 
-@translate.register(ast.FunctionDef)
-def _(node: ast.FunctionDef) -> str:
-    # TODO: Add default arguments support.
-    if len(node.args.kwonlyargs) != 0 or len(node.args.defaults) != 0:
-        log.warning(
-            "Unsupport function arguments: keyword only and default arguments are not supported!"
-        )
+@translate.register
+def _(node: ast.FunctionType) -> str:
+    return ""
+
+
+@translate.register
+def _(node: ast.Constant) -> str:
+    # None, str, bytes, bool, int, float, complex, Ellipsis
+    if node.value is None:
+        return "nil"
+    if node.value is Ellipsis:
         return ""
+    if isinstance(node.value, str):
+        return f'"{node.value}"'
+    if isinstance(node.value, bool):
+        return "true" if node.value else "false"
+    if isinstance(node.value, (int, float)):
+        return str(node.value)
+    log.warning(f"Unsupported constant: {node.value} is not supported!")
+    return ""
 
-    if len(node.decorator_list) != 0:
-        log.warning("Unsupport function: function with decorator is not supported!")
+
+@translate.register
+def _(node: ast.FormattedValue) -> str:
+    if node.conversion != -1 or node.format_spec is not None:
+        log.warning(f"Unsupported format string: value format is not supported!")
         return ""
-
-    func = Template("$funcname = {params [$args];$body};")
-    args = ",".join(f'"_{arg.arg}"' for arg in node.args.args)
-    body = "".join(translate(child) for child in node.body)
-    return func.substitute(funcname=node.name, args=args, body=body)
+    return translate(node.value)
 
 
-@translate.register(ast.Return)
-def _(node: ast.Return) -> str:
-    return f"{translate(node.value)}" if node.value is not None else ""
+@translate.register
+def _(node: ast.JoinedStr) -> str:
+    # No need to format but is a f-string :(
+    if all(isinstance(value, ast.Constant) for value in node.values):
+        return "".join(value.value for value in node.values)  # type: ignore
 
-
-@translate.register(ast.Delete)
-def _(node: ast.Delete) -> str:
+    index = 1
+    formatted_value = []
     syntax = []
-    for target in node.targets:
-        if not isinstance(target, ast.Name):
+    for value in node.values:
+        if isinstance(value, ast.Constant):
+            # The type of `value.value` is `str`
+            syntax.append(value.value)
+        else:
+            # The type of `value` is `ast.FormattedValue`
+            formatted_value.append(translate(value))
+            syntax.append(f"%{index}")
+            index += 1
+    return f"format [\"{''.join(syntax)}\", {', '.join(formatted_value)}]"
+
+
+@translate.register
+def _(node: ast.List | ast.Tuple) -> str:
+    elems = ", ".join(translate(elem) for elem in node.elts)
+    return f"[{elems}]"
+
+
+@translate.register
+def _(node: ast.Dict) -> str:
+    if any(key is None for key in node.keys):
+        log.warning("Unsupported dict: unpacking operator is not supported!")
+        return ""
+
+    array = ", ".join(
+        f"[{translate(k), translate(v)}]" for k, v in zip(node.keys, node.values)
+    )
+    return f"createHashMapFromArray [{array}]"
+
+
+@translate.register
+def _(node: ast.Name) -> str:
+    return f"_{node.id}"
+
+
+@translate.register
+def _(node: ast.Expr) -> str:
+    return f"{translate(node.value)};"
+
+
+@translate.register
+def _(node: ast.UnaryOp) -> str:
+    return f"{translate(node.op)}{translate(node.operand)}"
+
+
+@translate.register
+def _(node: ast.Not) -> str:
+    return "!"
+
+
+@translate.register
+def _(node: ast.BinOp) -> str:
+    return f"{translate(node.left)} {translate(node.op)} {translate(node.right)}"
+
+
+@translate.register
+def _(node: ast.Add) -> str:
+    return "+"
+
+
+@translate.register
+def _(node: ast.Sub) -> str:
+    return "-"
+
+
+@translate.register
+def _(node: ast.Mult) -> str:
+    return "*"
+
+
+@translate.register
+def _(node: ast.Div) -> str:
+    return "/"
+
+
+@translate.register
+def _(node: ast.FloorDiv) -> str:
+    return "/"
+
+
+@translate.register
+def _(node: ast.Mod) -> str:
+    return "mod"
+
+
+@translate.register
+def _(node: ast.Pow) -> str:
+    return "^"
+
+
+@translate.register
+def _(node: ast.BoolOp) -> str:
+    return translate(node.op).join(translate(child) for child in node.values)
+
+
+@translate.register
+def _(node: ast.And) -> str:
+    return " && "
+
+
+@translate.register
+def _(node: ast.Or) -> str:
+    return " || "
+
+
+@translate.register
+def _(node: ast.Compare) -> str:
+    if len(node.comparators) != 1:
+        log.warning("Unsupported compare: multiple compare is not supported!")
+        return ""
+    return f"{translate(node.left)} {translate(node.ops[0])} {translate(node.comparators[0])}"
+
+
+@translate.register
+def _(node: ast.Eq) -> str:
+    return "=="
+
+
+@translate.register
+def _(node: ast.NotEq) -> str:
+    return "!="
+
+
+@translate.register
+def _(node: ast.Lt) -> str:
+    return "<"
+
+
+@translate.register
+def _(node: ast.LtE) -> str:
+    return "<="
+
+
+@translate.register
+def _(node: ast.Gt) -> str:
+    return ">"
+
+
+@translate.register
+def _(node: ast.GtE) -> str:
+    return ">="
+
+
+@translate.register
+def _(node: ast.Call) -> str:
+    if len(node.keywords) != 0:
+        log.warning("Unsupported function call: keyword argument is not supported!")
+        return ""
+
+    if any(isinstance(arg, ast.Starred) for arg in node.args):
+        log.warning("Unsupported function call: unpacking operator is not supported!")
+        return ""
+
+    if isinstance(node.func, ast.Name):
+        if len(node.args) == 1:
+            return f"{translate(node.args[0])} call {node.func.id}"
+        args = ", ".join(translate(arg) for arg in node.args)
+        return f"[{args}] call {node.func.id}"
+
+    if isinstance(node.func, ast.Attribute):
+        if len(node.args) == 1:
+            return f"{translate(node.func)} {translate(node.args[0])}"
+        args = ", ".join(translate(arg) for arg in node.args)
+        return f"{translate(node.func)} [{args}]"
+
+    log.warning(f"Unsupported function call: {node.func} is not supported!")
+    return ""
+
+
+@translate.register
+def _(node: ast.IfExp) -> str:
+    ifelse = Template("if ($condition) then {$ifbody} else {$elsebody};")
+    condition = translate(node.test)
+    ifbody = translate(node.body)
+    elsebody = translate(node.orelse)
+    return ifelse.substitute(condition=condition, ifbody=ifbody, elsebody=elsebody)
+
+
+@translate.register
+def _(node: ast.Attribute) -> str:
+    # Special mark: GLOBAL
+    if isinstance(node.value, ast.Name) and node.value.id == "GLOBAL":
+        return node.attr
+    return f"{translate(node.value)} {node.attr}"
+
+
+@translate.register
+def _(node: ast.Subscript) -> str:
+    log.warning(
+        "For subscript, you should manually choose the right sentence.\n"
+        "For list/tuple, it's select. For dict, it's get.\n"
+        "If you want to assign, please use set instead."
+    )
+    if isinstance(node.slice, ast.Constant):
+        if isinstance(node.slice.value, int):
+            return f"{translate(node.value)} [select|get] {node.slice.value}"
+        return f"{translate(node.value)} get {node.slice.value}"
+
+    if isinstance(node.slice, ast.Slice):
+        if node.slice.step is not None:
+            log.warning("Unsupported subscript: slice with step is not supported!")
+            return ""
+
+        if node.slice.lower is None or node.slice.upper is None:
             log.warning(
-                f"Unsupported delete: Deleting a non-name target is not supported!"
+                "Unsupported subscript: slice without lower or upper is not supported!"
             )
             return ""
 
-        syntax.append(f"_{target.id} = nil;")
-    return "".join(syntax)
+        assert isinstance(node.slice.lower, ast.Constant) and isinstance(
+            node.slice.upper, ast.Constant
+        )
+        return f"{translate(node.value)} get [{node.slice.lower.value}, {node.slice.upper.value - node.slice.lower.value}]"
+
+    log.warning("Unsupported subscript: multiple slice is not supported!")
+    return ""
 
 
-@translate.register(ast.Assign)
-@translate.register(ast.AnnAssign)
+@translate.register
 def _(node: ast.Assign | ast.AnnAssign) -> str:
     rhs = node.value
     if rhs is None:
@@ -142,32 +358,61 @@ def _(node: ast.Assign | ast.AnnAssign) -> str:
     return "".join(syntax)
 
 
-@translate.register(ast.AugAssign)
+@translate.register
 def _(node: ast.AugAssign) -> str:
     # `lhs.ctx` must be `Store()` because it must have `rhs`.
     lhs = node.target
-    if not isinstance(lhs, ast.Name):
-        raise RuntimeError(f"Unknown error happened in parsing {node}!")
+    assert isinstance(lhs, ast.Name)
     return f"_{lhs.id} = _{lhs.id} {translate(node.op)} {translate(node.value)};"
 
 
-@translate.register(ast.For)
+@translate.register
+def _(node: ast.Delete) -> str:
+    syntax = []
+    for target in node.targets:
+        if not isinstance(target, ast.Name):
+            log.warning(
+                f"Unsupported delete: Deleting a non-name target is not supported!"
+            )
+            return ""
+
+        syntax.append(f"_{target.id} = nil;")
+    return "".join(syntax)
+
+
+@translate.register
+def _(node: ast.Pass) -> str:
+    return ""
+
+
+@translate.register
+def _(node: ast.If) -> str:
+    if len(node.orelse) != 0:
+        ifelse = Template("if ($condition) then {$ifbody} else {$elsebody};")
+        condition = translate(node.test)
+        ifbody = "".join(translate(child) for child in node.body)
+        elsebody = "".join(translate(child) for child in node.orelse)
+        return ifelse.substitute(condition=condition, ifbody=ifbody, elsebody=elsebody)
+    pureif = Template("if ($condition) then {$body};")
+    condition = translate(node.test)
+    body = "".join(translate(child) for child in node.body)
+    return pureif.substitute(condition=condition, body=body)
+
+
+@translate.register
 def _(node: ast.For) -> str:
     if len(node.orelse) != 0:
         log.warning("Unsupported or-else: else after for is not supported!")
         return ""
 
     # Special function: `range`.
-    # TODO: find a better way to finish this.
     if (
         isinstance(node.iter, ast.Call)
         and isinstance(node.iter.func, ast.Name)
         and node.iter.func.id == "range"
     ):
         forrange = Template('for "$val" from $start to $stop step $step do{$body};')
-        if not all(isinstance(arg, ast.Constant) for arg in node.iter.args):
-            raise RuntimeError(f"Unknown error happened in parsing {node}!")
-
+        assert all(isinstance(arg, ast.Constant) for arg in node.iter.args)
         if not isinstance(node.target, ast.Name):
             log.warning(
                 "Unsupported variable: for-range loop can only have one variable!"
@@ -203,7 +448,7 @@ def _(node: ast.For) -> str:
     return foreach.substitute(target=target, body=body, iter=iter)
 
 
-@translate.register(ast.While)
+@translate.register
 def _(node: ast.While) -> str:
     if len(node.orelse) != 0:
         log.warning("Unsupported or-else: else after while is not supported!")
@@ -214,209 +459,40 @@ def _(node: ast.While) -> str:
     return whileloop.substitute(condition=condition, body=body)
 
 
-@translate.register(ast.If)
-def _(node: ast.If) -> str:
-    if len(node.orelse) != 0:
-        ifelse = Template("if ($condition) then {$ifbody} else {$elsebody};")
-        condition = translate(node.test)
-        ifbody = "".join(translate(child) for child in node.body)
-        elsebody = "".join(translate(child) for child in node.orelse)
-        return ifelse.substitute(condition=condition, ifbody=ifbody, elsebody=elsebody)
-    pureif = Template("if ($condition) then {$body};")
-    condition = translate(node.test)
-    body = "".join(translate(child) for child in node.body)
-    return pureif.substitute(condition=condition, body=body)
+@translate.register
+def _(node: ast.Break) -> str:
+    return "break;"
 
 
-@translate.register(ast.Expr)
-def _(node: ast.Expr) -> str:
-    return f"{translate(node.value)};"
+@translate.register
+def _(node: ast.Continue) -> str:
+    return "continue;"
 
 
-@translate.register(ast.Pass)
-def _(node: ast.Pass) -> str:
-    return ""
-
-
-@translate.register(ast.BoolOp)
-def _(node: ast.BoolOp) -> str:
-    return translate(node.op).join(translate(child) for child in node.values)
-
-
-@translate.register(ast.And)
-def _(node: ast.And) -> str:
-    return " && "
-
-
-@translate.register(ast.Or)
-def _(node: ast.Or) -> str:
-    return " || "
-
-
-@translate.register(ast.BinOp)
-def _(node: ast.BinOp) -> str:
-    return f"{translate(node.left)} {translate(node.op)} {translate(node.right)}"
-
-
-@translate.register(ast.Add)
-def _(node: ast.Add) -> str:
-    return "+"
-
-
-@translate.register(ast.Sub)
-def _(node: ast.Sub) -> str:
-    return "-"
-
-
-@translate.register(ast.Mult)
-def _(node: ast.Mult) -> str:
-    return "*"
-
-
-@translate.register(ast.Div)
-def _(node: ast.Div) -> str:
-    return "/"
-
-
-@translate.register(ast.FloorDiv)
-def _(node: ast.FloorDiv) -> str:
-    return "/"
-
-
-@translate.register(ast.Mod)
-def _(node: ast.Mod) -> str:
-    return "mod"
-
-
-@translate.register(ast.Pow)
-def _(node: ast.Pow) -> str:
-    return "^"
-
-
-@translate.register(ast.UnaryOp)
-def _(node: ast.UnaryOp) -> str:
-    return f"{translate(node.op)}{translate(node.operand)}"
-
-
-@translate.register(ast.Not)
-def _(node: ast.Not) -> str:
-    return "!"
-
-
-@translate.register(ast.IfExp)
-def _(node: ast.IfExp) -> str:
-    ifelse = Template("if ($condition) then {$ifbody} else {$elsebody};")
-    condition = translate(node.test)
-    ifbody = translate(node.body)
-    elsebody = translate(node.orelse)
-    return ifelse.substitute(condition=condition, ifbody=ifbody, elsebody=elsebody)
-
-
-@translate.register(ast.Dict)
-def _(node: ast.Dict) -> str:
-    # TODO: Support dict.
-    log.warning("Unsupported dict: dict is not supported!")
-    return ""
-
-
-@translate.register(ast.Compare)
-def _(node: ast.Compare) -> str:
-    if len(node.comparators) != 1:
-        log.warning("Unsupported compare: multiple compare is not supported!")
-        return ""
-    return f"{translate(node.left)} {translate(node.ops[0])} {translate(node.comparators[0])}"
-
-
-@translate.register(ast.Eq)
-def _(node: ast.Eq) -> str:
-    return "=="
-
-
-@translate.register(ast.NotEq)
-def _(node: ast.NotEq) -> str:
-    return "!="
-
-
-@translate.register(ast.Lt)
-def _(node: ast.Lt) -> str:
-    return "<"
-
-
-@translate.register(ast.LtE)
-def _(node: ast.LtE) -> str:
-    return "<="
-
-
-@translate.register(ast.Gt)
-def _(node: ast.Gt) -> str:
-    return ">"
-
-
-@translate.register(ast.GtE)
-def _(node: ast.GtE) -> str:
-    return ">="
-
-
-@translate.register(ast.Call)
-def _(node: ast.Call) -> str:
-    if len(node.keywords) != 0:
-        log.warning("Unsupported function call: keyword argument is not supported!")
-        return ""
-
-    if any(isinstance(arg, ast.Starred) for arg in node.args):
+@translate.register
+def _(node: ast.FunctionDef | ast.AsyncFunctionDef) -> str:
+    # TODO: Add default arguments support.
+    if len(node.args.kwonlyargs) != 0 or len(node.args.defaults) != 0:
         log.warning(
-            "Unsupported function call: unpacking positional argument is not supported!"
+            "Unsupport function arguments: keyword only and default arguments are not supported!"
         )
         return ""
 
-    if isinstance(node.func, ast.Name):
-        if len(node.args) == 1:
-            return f"{translate(node.args[0])} call {node.func.id}"
-        args = ", ".join(translate(arg) for arg in node.args)
-        return f"[{args}] call {node.func.id}"
-
-    if isinstance(node.func, ast.Attribute):
-        if len(node.args) == 1:
-            return f"{translate(node.func)} {translate(node.args[0])}"
-        args = ", ".join(translate(arg) for arg in node.args)
-        return f"{translate(node.func)} [{args}]"
-
-    log.warning(f"Unsupported function call: {node.func} is not supported!")
-    return ""
-
-
-@translate.register(ast.Constant)
-def _(node: ast.Constant) -> str:
-    # None, str, bytes, bool, int, float, complex, Ellipsis
-    if node.value is None:
-        return "nil"
-    if node.value is Ellipsis:
+    if len(node.decorator_list) != 0:
+        log.warning("Unsupport function: function with decorator is not supported!")
         return ""
-    if isinstance(node.value, str):
-        return f'"{node.value}"'
-    if isinstance(node.value, bool):
-        return "true" if node.value else "false"
-    if isinstance(node.value, (int, float)):
-        return str(node.value)
-    log.warning(f"Unsupported constant: {node.value} is not supported")
-    return ""
+
+    func = Template("$funcname = {params [$args];$body};")
+    args = ", ".join(f'"_{arg.arg}"' for arg in node.args.args)
+    body = "".join(translate(child) for child in node.body)
+    return func.substitute(funcname=node.name, args=args, body=body)
 
 
-@translate.register(ast.Attribute)
-def _(node: ast.Attribute) -> str:
-    # Special mark: GLOBAL
-    if isinstance(node.value, ast.Name) and node.value.id == "GLOBAL":
-        return node.attr
-    return f"{translate(node.value)} {node.attr}"
+@translate.register
+def _(node: ast.Return) -> str:
+    return f"{translate(node.value)}" if node.value is not None else ""
 
 
-@translate.register(ast.Name)
-def _(node: ast.Name) -> str:
-    return f"_{node.id}"
-
-
-@translate.register(ast.List)
-@translate.register(ast.Tuple)
-def _(node: ast.List | ast.Tuple) -> str:
-    elems = ", ".join(translate(elem) for elem in node.elts)
-    return f"[{elems}]"
+@translate.register
+def _(node: ast.Await) -> str:
+    return f"waitUntil {{{translate(node.value)}}};"
