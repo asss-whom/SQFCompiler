934f8eb2a31b9bd2c6f773006991932b979c24d2
asss-whom <3059749821@qq.com>
2024-05-12 19:36:29 +0800

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,6 @@ def can_bomb(bomber, target):
     height = GLOBAL.getPosATL(bomber)[2]
     distance = bomber.distance2D(target)
     speed = GLOBAL.speed(bomber) * 1000 / (60 * 60)
-
     time = (2 * height / 9.8) ** 0.5
     return distance <= speed * time
 
@@ -10,6 +9,9 @@ def can_bomb(bomber, target):
 async def drop_bomb(bomber, target, weapon):
     await can_bomb(bomber, target)
 
-    while bomber.ammo(weapon) != 0:
+    bomber.selectWeapon(weapon)
+    while bomber.ammo(weapon) >= 0:
         BIS_fnc_fire(bomber, weapon)
-        GLOBAL.sleep(0.1)
+
+
+drop_bomb(GLOBAL.this, GLOBAL.target, "BombCluster_02_F")
