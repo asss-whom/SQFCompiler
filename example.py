2980b926b9eb3d205da241d060b97458aecc510f
Asss_whom <cxfan2013@qq.com>
2026-01-07 22:39:46 +0800

    Change example to a better version.

--- example.py
+++ example.py
@@ -1,22 +1,12 @@
+jet = GLOBAL.vehicle(GLOBAL.this)
 for obj in GLOBAL.vehicles:
-    if GLOBAL.side(GLOBAL.player).getFriend(GLOBAL.side(obj)) < 0.6:
-        if obj.isKindOf("Land") or obj.isKindOf("Ship"):
-            missile = GLOBAL.createVehicle(
-                "Missile_AGM_01_F",
-                GLOBAL.player.modelToWorld(0, 0, 100),
-                [],
-                0,
-                "CAN_COLLIDE",
-            )
-            missile.setDir(GLOBAL.getDir(GLOBAL.player))
-            missile.setMissileTarget(obj, True)
-        if obj.isKindOf("Air"):
-            missile = GLOBAL.createVehicle(
-                "Missile_AA_03_F",
-                GLOBAL.player.modelToWorld(0, 0, 100),
-                [],
-                0,
-                "CAN_COLLIDE",
-            )
-            missile.setDir(GLOBAL.getDir(GLOBAL.player))
-            missile.setMissileTarget(obj, True)
+    if GLOBAL.side(GLOBAL.this).getFriend(GLOBAL.side(obj)) < 0.6:
+        missile = GLOBAL.createVehicle(
+            "ammo_Missile_Cruise_01",
+            jet.modelToWorld(0, 0, -5),
+            [],
+            0,
+            "CAN_COLLIDE",
+        )
+        missile.setDir(GLOBAL.getDir(jet))
+        missile.setMissileTarget(obj, True)
--- example.sqf
+++ example.sqf
@@ -1,15 +1,9 @@
+_jet = vehicle this;
 {
     private _obj = _x;
-    if (side player getFriend side _obj < 0.6) then {
-        if (_obj isKindOf "Land" || _obj isKindOf "Ship") then {
-            _missile = createVehicle ["Missile_AGM_01_F", player modelToWorld [0, 0, 100], [], 0, "CAN_COLLIDE"];
-            _missile setDir getDir player;
-            _missile setMissileTarget [_obj, true];
-        };
-        if (_obj isKindOf "Air") then {
-            _missile = createVehicle ["Missile_AA_03_F", player modelToWorld [0, 0, 100], [], 0, "CAN_COLLIDE"];
-            _missile setDir getDir player;
-            _missile setMissileTarget [_obj, true];
-        };
+    if (side this getFriend side _obj < 0.6) then {
+        _missile = createVehicle ["ammo_Missile_Cruise_01", _jet modelToWorld [0, 0, -5], [], 0, "CAN_COLLIDE"];
+        _missile setDir getDir _jet;
+        _missile setMissileTarget [_obj, true];
     };
 } forEach vehicles;
