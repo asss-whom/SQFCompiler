6e3bc8df39a856b2fd11d61a2b25bafb4fc5570e
Asss_whom <cxfan2013@qq.com>
2026-01-15 17:14:18 +0800

    A complex demo.

--- example.py
+++ example.py
@@ -1,22 +1,66 @@
+jet = GLOBAL.vehicle(GLOBAL.this)
 for target in GLOBAL.vehicles:
-    if GLOBAL.side(GLOBAL.player).getFriend(GLOBAL.side(target)) < 0.6:
-        if target.isKindOf("Land") or target.isKindOf("Ship"):
-            missile = GLOBAL.createVehicle(
-                "ammo_Missile_Cruise_01",
-                GLOBAL.player.modelToWorld(0, 0, 100),
-                [],
-                0,
-                "CAN_COLLIDE",
-            )
-            missile.setDir(GLOBAL.player.getDir(target))
-            missile.setMissileTarget(target, True)
-        if target.isKindOf("Air"):
-            missile = GLOBAL.createVehicle(
-                "ammo_Missile_mim145",
-                GLOBAL.player.modelToWorld(0, 0, 100),
-                [],
-                0,
-                "CAN_COLLIDE",
-            )
-            missile.setDir(GLOBAL.player.getDir(target))
-            missile.setMissileTarget(target, True)
+    if GLOBAL.side(GLOBAL.this).getFriend(GLOBAL.side(target)) < 0.6:
+        if GLOBAL.side(GLOBAL.this) == GLOBAL.blufor:
+            if target.isKindOf("Land") or target.isKindOf("Ship"):
+                missile = GLOBAL.createVehicle(
+                    "Missile_AGM_02_F",
+                    jet.modelToWorld(0, 0, -5),
+                    [],
+                    0,
+                    "CAN_COLLIDE",
+                )
+                missile.setDir(GLOBAL.getDir(jet))
+                missile.setMissileTarget(target, True)
+            if target.isKindOf("Air"):
+                missile = GLOBAL.createVehicle(
+                    "ammo_Missile_AMRAAM_D",
+                    jet.modelToWorld(0, 0, -5),
+                    [],
+                    0,
+                    "CAN_COLLIDE",
+                )
+                missile.setDir(GLOBAL.getDir(jet))
+                missile.setMissileTarget(target, True)
+        if GLOBAL.side(GLOBAL.this) == GLOBAL.opfor:
+            if target.isKindOf("Land") or target.isKindOf("Ship"):
+                missile = GLOBAL.createVehicle(
+                    "Missile_AGM_01_F",
+                    jet.modelToWorld(0, 0, -5),
+                    [],
+                    0,
+                    "CAN_COLLIDE",
+                )
+                missile.setDir(GLOBAL.getDir(jet))
+                missile.setMissileTarget(target, True)
+            if target.isKindOf("Air"):
+                missile = GLOBAL.createVehicle(
+                    "ammo_Missile_AA_R77",
+                    jet.modelToWorld(0, 0, -5),
+                    [],
+                    0,
+                    "CAN_COLLIDE",
+                )
+                missile.setDir(GLOBAL.getDir(jet))
+                missile.setMissileTarget(target, True)
+        if GLOBAL.side(GLOBAL.this) == GLOBAL.independent:
+            if target.isKindOf("Land") or target.isKindOf("Ship"):
+                missile = GLOBAL.createVehicle(
+                    "Missile_AGM_02_F",
+                    jet.modelToWorld(0, 0, -5),
+                    [],
+                    0,
+                    "CAN_COLLIDE",
+                )
+                missile.setDir(GLOBAL.getDir(jet))
+                missile.setMissileTarget(target, True)
+            if target.isKindOf("Air"):
+                missile = GLOBAL.createVehicle(
+                    "ammo_Missile_AMRAAM_C",
+                    jet.modelToWorld(0, 0, -5),
+                    [],
+                    0,
+                    "CAN_COLLIDE",
+                )
+                missile.setDir(GLOBAL.getDir(jet))
+                missile.setMissileTarget(target, True)
--- example.sqf
+++ example.sqf
@@ -1,15 +1,42 @@
+_jet = vehicle this;
 {
     private _target = _x;
-    if (side player getFriend side _target < 0.6) then {
-        if (_target isKindOf "Land" || _target isKindOf "Ship") then {
-            _missile = createVehicle ["ammo_Missile_Cruise_01", player modelToWorld [0, 0, 100], [], 0, "CAN_COLLIDE"];
-            _missile setDir player getDir _target;
-            _missile setMissileTarget [_target, true];
+    if (side this getFriend side _target < 0.6) then {
+        if (side this == blufor) then {
+            if (_target isKindOf "Land" || _target isKindOf "Ship") then {
+                _missile = createVehicle ["Missile_AGM_02_F", _jet modelToWorld [0, 0, -5], [], 0, "CAN_COLLIDE"];
+                _missile setDir getDir _jet;
+                _missile setMissileTarget [_target, true];
+            };
+            if (_target isKindOf "Air") then {
+                _missile = createVehicle ["ammo_Missile_AMRAAM_D", _jet modelToWorld [0, 0, -5], [], 0, "CAN_COLLIDE"];
+                _missile setDir getDir _jet;
+                _missile setMissileTarget [_target, true];
+            };
         };
-        if (_target isKindOf "Air") then {
-            _missile = createVehicle ["ammo_Missile_mim145", player modelToWorld [0, 0, 100], [], 0, "CAN_COLLIDE"];
-            _missile setDir player getDir _target;
-            _missile setMissileTarget [_target, true];
+        if (side this == opfor) then {
+            if (_target isKindOf "Land" || _target isKindOf "Ship") then {
+                _missile = createVehicle ["Missile_AGM_01_F", _jet modelToWorld [0, 0, -5], [], 0, "CAN_COLLIDE"];
+                _missile setDir getDir _jet;
+                _missile setMissileTarget [_target, true];
+            };
+            if (_target isKindOf "Air") then {
+                _missile = createVehicle ["ammo_Missile_AA_R77", _jet modelToWorld [0, 0, -5], [], 0, "CAN_COLLIDE"];
+                _missile setDir getDir _jet;
+                _missile setMissileTarget [_target, true];
+            };
+        };
+        if (side this == independent) then {
+            if (_target isKindOf "Land" || _target isKindOf "Ship") then {
+                _missile = createVehicle ["Missile_AGM_02_F", _jet modelToWorld [0, 0, -5], [], 0, "CAN_COLLIDE"];
+                _missile setDir getDir _jet;
+                _missile setMissileTarget [_target, true];
+            };
+            if (_target isKindOf "Air") then {
+                _missile = createVehicle ["ammo_Missile_AMRAAM_C", _jet modelToWorld [0, 0, -5], [], 0, "CAN_COLLIDE"];
+                _missile setDir getDir _jet;
+                _missile setMissileTarget [_target, true];
+            };
         };
     };
 } forEach vehicles;
